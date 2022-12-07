507ec49c1e54619b7886f7a5bbf9e928a625c1c0
Sharona Young <msrona806@gmail.com>
2022-12-07 18:58:36 -0500

    updated code

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
 const input = document.querySelector('#itemInput');
 const ul = document.querySelector('ul')
-
 let items = [];
-
 
 document.querySelector('form').addEventListener('submit', function(event){
   event.preventDefault();
-  console.log("Submit clicked");
-  
+   
   // add item entered to items array
-  items.push(input.value);
-  
+  items.push({
+    name: input.value,
+    complete: false
+  });
   // clear the input box
   input.value = "";
-
-  // function display items added to a list
   showItems();
- 
-});
+ });
+
+// function display items added to a list
 const showItems = () => {
   const todoList = items.map((item, index) =>
   `
-    <li><input type="checkbox" data-index="${index}" id="done"/>${item}  <button data-index="${index}" id="delete">x</button></li> 
+    <li><input type="checkbox" data-index="${index}" id="done"/>${item.name}  <button data-index="${index}" id="delete">x</button></li> 
   `
   )
   ul.innerHTML = todoList.join('');
 }
 
-// event listener for button click
-ul.addEventListener('click', function(event){
-  
-  if(event.target.matches('input')){
-    const checkIndex = event.target.dataset.index;
-    let checkbox = document.querySelector("done");
-    checkbox = true;
-    console.log(checkIndex + ' checkbox clicked');   
 
-  } else if(event.target.matches('#delete')){
-    const buttonIndex = event.target.dataset.index 
-    console.log(items[buttonIndex]);
-    items.splice(buttonIndex,1);
-  }
-  showItems()
+
+ul.addEventListener('click', function(event){
+  const index = event.target.dataset.index;
+  // when checkbox is clicked
+  if(event.target.matches('input')){
+    items[index].complete = !items[index].complete
+    console.log(`checkbox clicked for index ${event.target.dataset.index}`);
+    
+    // *** only adds to first item, this.querySelectorAll doesn't work ***
+    /* const checkbox = document.querySelector("ul li");
+    checkbox.classList.add("done"); */
+
+    // ***does not work***
+    // items[index].style.textDecoration('line-through');
+    
+
+  // when delete buotton clicked
+  }else if(event.target.matches('button')){
+    console.log(`delete clicked for index  ${event.target.dataset.index}`);
+    items.splice(index,1);
+    showItems();
+  }  
 });
